7439b0428d3794220c592e017527f13ed8ec007e
Will Dady <will@good.work>
2019-06-16 22:18:35 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# terraform-transmission-aws
+
+A [Terraform](terraform) configuration for provisioning an EC2 instance and runs [Transmission](transmission) over [OpenVPN](openvpn) with completed downloads automatically copied to an S3 bucket.
+
+This is intended for ephemeral deployments where you may spin-up an instance for an hour or two and then destroy it. It's not intended for long-running use.
+
+## Prerequisites
+
+In order to use this configuration you MUST have:
+
+- An [AWS](aws) account
+- An existing EC2 keypair
+- An existing S3 bucket
+- AWS credentials on the machine you will run `terraform apply` from
+- A supported VPN provider (see providers [here](https://haugene.github.io/docker-transmission-openvpn/supported-providers/))
+- An understanding of [Terraform](terraform) and it installed on your system
+
+## Setup
+
+Initialise terraform
+
+```bash
+terraform init
+```
+
+You will need to provide values for all variables defined at the top of `stack.tf`. The easy way to do this is by creating a `terraform.tfvars` file. See the [terraform docs](https://www.terraform.io/docs/configuration/variables.html#variable-definitions-tfvars-files) for more information.
+
+Once you've defined your variables apply the configuration.
+
+```bash
+terraform apply
+```
+
+Once terraform is finished you will get the URL of the Transmission
+
+And once finished, destroy.
+
+```bash
+terraform destroy
+```
+
+[terraform]: https://www.terraform.io/
+[transmission]: https://transmissionbt.com/
+[openvpn]: https://openvpn.net/
+[aws]: https://aws.amazon.com/
---
+++ stack.tf
@@ -0,0 +1,148 @@
+variable "openvpn_provider" {}
+variable "openvpn_config" {}
+variable "openvpn_username" {}
+variable "openvpn_password" {}
+variable "aws_region" {}
+variable "aws_key_name" {}
+variable "aws_vpc_id" {}
+variable "aws_subnet_id" {}
+variable "aws_instance_type" {
+  default = "t2.micro"
+}
+variable "aws_ami" {}
+variable "aws_s3_bucket" {}
+
+output "webui" {
+  value = "http://${aws_instance.transmission_box.public_ip}:8080/transmission/web/"
+}
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+resource "aws_security_group" "transmission-sg" {
+  name_prefix = "transmission-sg"
+  description = "Access Transmission on port 8080"
+  vpc_id      = "${var.aws_vpc_id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "SSH"
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Transmission WebUI"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_role_policy" "transmission_box_role_policy" {
+  name_prefix = "transmission_box_role_policy_"
+  role        = "${aws_iam_role.transmission_box_role.id}"
+  policy      = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "RemoteTransmission0",
+            "Effect": "Allow",
+            "Action": [
+                "s3:*"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${var.aws_s3_bucket}/*",
+                "arn:aws:s3:::${var.aws_s3_bucket}"
+            ]
+        }
+    ]
+}
+  EOF
+}
+
+resource "aws_iam_role" "transmission_box_role" {
+  name_prefix = "transmission_box_role_"
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+              "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+  EOF
+}
+
+resource "aws_iam_instance_profile" "transmission_box_profile" {
+  name_prefix = "transmission_box_profile_"
+  role        = "${aws_iam_role.transmission_box_role.name}"
+}
+
+resource "aws_instance" "transmission_box" {
+  ami                    = "${var.aws_ami}"
+  instance_type          = "${var.aws_instance_type}"
+  subnet_id              = "${var.aws_subnet_id}"
+  vpc_security_group_ids = ["${aws_security_group.transmission-sg.id}"]
+  key_name               = "${var.aws_key_name}"
+  root_block_device {
+    volume_size = 10
+  }
+  iam_instance_profile = "${aws_iam_instance_profile.transmission_box_profile.name}"
+  user_data            = <<EOF
+    #!/bin/bash
+    yum update -y
+    amazon-linux-extras install docker -y
+    service docker start
+    usermod -a -G docker ec2-user
+    mkdir /opt/completed
+    docker pull haugene/transmission-openvpn
+    docker pull haugene/transmission-openvpn-proxy
+    docker pull willdady/go-watch-s3
+    docker run -d \
+      --cap-add=NET_ADMIN \
+      --name my_transmission \
+      -v /opt/:/data \
+      -v /etc/localtime:/etc/localtime:ro \
+      -e CREATE_TUN_DEVICE=true \
+      -e OPENVPN_PROVIDER=${var.openvpn_provider} \
+      -e "OPENVPN_CONFIG=${var.openvpn_config}" \
+      -e OPENVPN_USERNAME=${var.openvpn_username} \
+      -e OPENVPN_PASSWORD=${var.openvpn_password} \
+      -e WEBPROXY_ENABLED=false \
+      -e TRANSMISSION_SPEED_LIMIT_UP_ENABLED=true \
+      -e TRANSMISSION_SPEED_LIMIT_UP=10 \
+      --log-driver json-file \
+      --log-opt max-size=10m \
+      haugene/transmission-openvpn
+    docker run -d \
+      --name my_transmission_proxy \
+      --link my_transmission:transmission \
+      -p 8080:8080 \
+      haugene/transmission-openvpn-proxy
+    docker run -d \
+      --name my_go_watch_s3 \
+      -v /opt/completed:/data \
+      -e WATCH_PATH=/data/ \
+      -e AWS_REGION=${var.aws_region} \
+      -e AWS_S3_BUCKET=${var.aws_s3_bucket} \
+      willdady/go-watch-s3
+  EOF
+}
